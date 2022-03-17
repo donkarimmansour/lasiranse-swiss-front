7b6b5f2b5a5fa01b6c5ebf8517bcb4c18c4b71c6
karim wydadi <don.karimmansour@gmail.com>
2022-03-17 14:47:34 +0000

    new

--- src/common/apiEndPoints.js
+++ src/common/apiEndPoints.js
@@ -1,8 +1,7 @@
 
 const Host = {
   ROOT: "http://localhost:3000",
-  BACKEND: "http://localhost:3001",
-  //BACKEND: "https://api.compareprime.com",
+  BACKEND: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:3001" : "https://api.compareprime.com" ,
   PREFIX: "/v1/api", 
 };
 
